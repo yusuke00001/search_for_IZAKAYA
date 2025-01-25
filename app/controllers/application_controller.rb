1755c0b03a0691fb254fc39befee59a3c7dfeb73
yusuke00001 <volleyball.pipi@icloud.com>
2025-01-25 14:51:51 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
     remember_token = User.new_remember_token
     cookies.permanent[:user_remember_token] = remember_token
     user.update!(remember_token: User.encrypt(remember_token))
-    @current_user = user
   end
 
   def sign_out
